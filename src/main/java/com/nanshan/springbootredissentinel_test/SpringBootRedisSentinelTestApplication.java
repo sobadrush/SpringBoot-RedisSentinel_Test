1a4988f8c06cbba44f01555bd733b08e8f8a8332
Roger Lo <Roger-KF.Lo@NANSHAN.com.tw>
2023-10-27 09:14:49 +0800

    init commit

---
+++ src/main/java/com/nanshan/springbootredissentinel_test/SpringBootRedisSentinelTestApplication.java
@@ -0,0 +1,13 @@
+package com.nanshan.springbootredissentinel_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisSentinelTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisSentinelTestApplication.class, args);
+    }
+
+}
