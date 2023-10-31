25e436ef8f87a40dc9d9bf3af1a586d1fc9703b8
Roger Lo <Roger-KF.Lo@NANSHAN.com.tw>
2023-10-31 12:12:03 +0800

    新增測試：連接到已配置好 Master/Replica 的 Master 節點

--- src/test/java/com/nanshan/springbootredissentinel_test/redis/RedisSentinelTest.java
+++ src/test/java/com/nanshan/springbootredissentinel_test/redis/RedisSentinelTest.java
@@ -2,11 +2,13 @@ package com.nanshan.springbootredissentinel_test.redis;
 
 import com.nanshan.springbootredissentinel_test.BaseTest;
 import io.lettuce.core.*;
+import io.lettuce.core.api.async.RedisAsyncCommands;
 import io.lettuce.core.codec.StringCodec;
 import io.lettuce.core.masterreplica.MasterReplica;
 import io.lettuce.core.masterreplica.StatefulRedisMasterReplicaConnection;
 import io.lettuce.core.sentinel.api.StatefulRedisSentinelConnection;
 import io.lettuce.core.sentinel.api.sync.RedisSentinelCommands;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,10 +24,51 @@ public class RedisSentinelTest extends BaseTest {
     }
 
     @Test
-    @DisplayName("[test_002] 測試連線到 Redis Sentinel 本人")
+    @DisplayName("[test_002] Standalone Master/Replica")
+    @SneakyThrows
+    // @Disabled
+    public void test_002() {
+        // 連接到已配置好 Master/Replica 的 Master 節點
+        RedisURI uri = RedisURI.create("redis://sa123456@127.0.0.1:6379/0");
+        RedisClient redisClient = RedisClient.create(uri);
+        redisClient.setOptions(ClientOptions.builder()
+                .autoReconnect(true)
+                .pingBeforeActivateConnection(true)
+                .timeoutOptions(TimeoutOptions.builder().fixedTimeout(Duration.ofSeconds(5)).build())
+                .build());
+        try(StatefulRedisMasterReplicaConnection<String, String> conn
+                    = MasterReplica.connect(redisClient, StringCodec.UTF8, uri)) {
+            System.out.println(".......Connected to Redis.......");
+            conn.setReadFrom(ReadFrom.REPLICA_PREFERRED);
+
+            RedisAsyncCommands<String, String> asyncCmd = conn.async();
+            RedisFuture<String> future = asyncCmd.set("todo1", "{" +
+                    "  \"userId\": 1," +
+                    "  \"id\": 1," +
+                    "  \"title\": \"Clean the Room\"," +
+                    "  \"completed\": false" +
+                    "}");
+            future.thenAccept(result -> {
+                System.out.println("Set operation result: " + result);
+                // 檢查操作是否成功
+                if ("OK".equals(result)) {
+                    System.err.println("<<< 操作 Redis 成功!");
+                } else {
+                    System.err.println("<<< 操作 Redis 失敗!");
+                }
+            });
+            Thread.sleep(2000); // 等待一些時間，讓非同步的 callback 觸發
+        } finally {
+            System.out.println("....... call shutdown .......");
+            redisClient.shutdown();
+        }
+    }
+
+    @Test
+    @DisplayName("[test_003] 測試連線到 Redis Sentinel 本人")
     // @Disabled
     // ref. https://blog.csdn.net/FlyLikeButterfly/article/details/124496285
-    public void test_002() {
+    public void test_003() {
         //sentinel
         RedisURI sentinelUri = RedisURI.builder()
             .withSentinel("127.0.0.1", 26379, "sa12345") // 哨兵地址和密码
@@ -40,9 +83,9 @@ public class RedisSentinelTest extends BaseTest {
 
     // FixMe: Cannot connect Redis Sentinel at redis://127.0.0.1:26381
     @Test
-    @DisplayName("[test_003] 測試連線到 Redis Sentinel 使用 RedisURI.create")
+    @DisplayName("[test_004] 測試連線到 Redis Sentinel 使用 RedisURI.create")
     // @Disabled
-    public void test_003() {
+    public void test_004() {
         RedisURI uri = RedisURI.create("redis-sentinel://sa12345@127.0.0.1:26379,127.0.0.1:26380,127.0.0.1:26381/0#mymaster");
         // System.out.println(redisURI);
         RedisClient redisClient = RedisClient.create(uri);
